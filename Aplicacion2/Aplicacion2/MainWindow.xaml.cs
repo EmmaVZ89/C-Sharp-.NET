43e813166420051d164ca7612f7b945dd46adfa5
Emmanuel Zelarayan <emmavz@live.com>
2022-11-29 21:00:39 -0300

    add WPF

---
+++ Aplicacion2/Aplicacion2/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Aplicacion2
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //Grid grid = new Grid();
+            //this.Content = grid;
+
+            //Button btn = new Button();
+
+            //WrapPanel wp = new WrapPanel();
+
+            //TextBlock txt1 = new TextBlock();
+            //txt1.Text = "Click";
+            //txt1.Foreground = Brushes.Red;
+
+            //TextBlock txt2 = new TextBlock();
+            //txt2.Text = "Enviar";
+            //txt2.Foreground = Brushes.Blue;
+
+            //TextBlock txt3 = new TextBlock();
+            //txt3.Text = "Ahora";
+            //txt3.Foreground = Brushes.Yellow;
+
+            //wp.Children.Add(txt1);
+            //wp.Children.Add(txt2);
+            //wp.Children.Add(txt3);
+
+            //btn.Content = wp;
+
+            //btn.Width = 200;
+            //btn.Height = 100;
+            //btn.Background = Brushes.BlueViolet;
+
+            //grid.Children.Add(btn);
+
+
+        }
+
+        //private void Button_Click(object sender, RoutedEventArgs e)
+        //{
+        //    //MessageBox.Show("Click en el botón");
+        //    Console.WriteLine("Click en el botón");
+        //}
+
+        //private void Panel_Click(object sender, RoutedEventArgs e)
+        //{
+        //    Console.WriteLine("Click en el botón");
+        //}
+
+        //private void Panel_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        //{
+        //    Console.WriteLine("Click en el botón");
+        //}
+    }
+}
---
+++ Aplicacion3/Aplicacion3/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Aplicacion3
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //public int PruebaProperty
+        //{
+        //    get { return (int)GetValue(PruebaDependencyProperty); }
+        //    set { SetValue(PruebaDependencyProperty, value); }
+        //}
+
+        //// Registrar Property
+        //public static readonly DependencyProperty PruebaDependencyProperty = 
+        //    DependencyProperty.Register("PruebaProperty", typeof(int), typeof(MainWindow), new PropertyMetadata(0));
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
