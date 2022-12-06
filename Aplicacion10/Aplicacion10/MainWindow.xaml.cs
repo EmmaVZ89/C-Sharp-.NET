ee3c963c6018e570bc6c33d85b8370516dcc9190
Emmanuel Zelarayan <emmavz@live.com>
2022-12-06 16:13:53 -0300

    add linq ORM

---
+++ Aplicacion10/Aplicacion10/MainWindow.xaml.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Configuration;
+
+namespace Aplicacion10
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DataClasses1DataContext dataContext;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string conexion = ConfigurationManager.ConnectionStrings["Aplicacion10.Properties.Settings.CrudLinqSql"].ConnectionString;
+
+            this.dataContext = new DataClasses1DataContext(conexion);
+
+            // CRUD con LINQ ORM
+            //this.CrearEmpresa();
+            //this.CrearEmpleado();
+            //this.CrearCargo();
+            //this.RelacionarEmpleadoCargo();
+            //this.ActualizarEmpleado();
+            //this.EliminarEmpleado();
+        }
+
+
+        public void CrearEmpresa()
+        {
+            //this.dataContext.ExecuteCommand("DELETE FROM empresas");
+
+            empresas empresa = new empresas();
+            empresa.Nombre = "Twitter";
+
+            this.dataContext.empresas.InsertOnSubmit(empresa);
+            this.dataContext.SubmitChanges();//se confirman los cambios
+
+            this.Principal.ItemsSource = this.dataContext.empresas;
+        }
+
+        public void CrearEmpleado()
+        {
+            empresas empresa1 = this.dataContext.empresas.First(emp => emp.Nombre.Equals("Google"));
+            empresas empresa2 = this.dataContext.empresas.First(emp => emp.Nombre.Equals("Facebook"));
+
+            List<empleados> listaEmpleados = new List<empleados>();
+            listaEmpleados.Add(new empleados { Nombre = "Emma", Apellido = "Perez", IdEmpresa = empresa1.Id });
+            listaEmpleados.Add(new empleados { Nombre = "Juan", Apellido = "Gomez", IdEmpresa = empresa2.Id });
+            listaEmpleados.Add(new empleados { Nombre = "Maria", Apellido = "Torrez", IdEmpresa = empresa1.Id });
+            listaEmpleados.Add(new empleados { Nombre = "Pedro", Apellido = "Dias", IdEmpresa = empresa2.Id });
+
+            this.dataContext.empleados.InsertAllOnSubmit(listaEmpleados);
+            this.dataContext.SubmitChanges();
+
+            this.Principal.ItemsSource = this.dataContext.empleados;
+        }
+
+        public void CrearCargo()
+        {
+            cargos cargo = new cargos();
+            cargo.Cargo = "Administrativo";
+
+            this.dataContext.cargos.InsertOnSubmit(cargo);
+            this.dataContext.SubmitChanges();//se confirman los cambios
+
+            this.Principal.ItemsSource = this.dataContext.cargos;
+        }
+
+        public void RelacionarEmpleadoCargo()
+        {
+            //empleados emp1 = this.dataContext.empleados.First(emp => emp.Nombre.Equals("Emma"));
+            //empleados emp2 = this.dataContext.empleados.First(emp => emp.Nombre.Equals("Juan"));
+            //cargos empDirector = this.dataContext.cargos.First(car => car.Cargo.Equals("Director"));
+            //cargos empAdministrativo = this.dataContext.cargos.First(car => car.Cargo.Equals("Administrativo"));
+            //cargos_empleados cargoEmp1 = new cargos_empleados();
+            //cargoEmp1.empleados = emp1;
+            //cargoEmp1.IdCargo = empDirector.Id;
+            //cargos_empleados cargoEmp2 = new cargos_empleados();
+            //cargoEmp2.empleados = emp2;
+            //cargoEmp2.IdCargo = empAdministrativo.Id;
+
+            List<cargos_empleados> listaCargosEmpleados = new List<cargos_empleados>();
+            listaCargosEmpleados.Add(new cargos_empleados { IdCargo = 1, IdEmpleado = 3 });
+            listaCargosEmpleados.Add(new cargos_empleados { IdCargo = 3, IdEmpleado = 4 });
+
+            this.dataContext.cargos_empleados.InsertAllOnSubmit(listaCargosEmpleados);
+
+            this.dataContext.SubmitChanges();
+
+            this.Principal.ItemsSource = this.dataContext.cargos_empleados;
+            
+        }
+
+        public void ActualizarEmpleado()
+        {
+            empleados empleado = this.dataContext.empleados.First(emp => emp.Nombre.Equals("Emma"));
+            empleado.Nombre = "Emma Victor";
+
+            this.dataContext.SubmitChanges();
+
+            this.Principal.ItemsSource = this.dataContext.empleados;
+        }
+
+        public void EliminarEmpleado()
+        {
+            empleados empleado = this.dataContext.empleados.First(emp => emp.Nombre.Equals("Juan"));
+
+            this.dataContext.empleados.DeleteOnSubmit(empleado);
+            this.dataContext.SubmitChanges();
+
+            this.Principal.ItemsSource = this.dataContext.empleados;
+        }
+    }
+}
---
+++ Aplicacion9/Aplicacion9/Program.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace Aplicacion9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] numeros = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            //Console.WriteLine("Números pares: ");
+            //List<int> numerosPares = new List<int>();
+            //foreach (int num in numeros)
+            //{
+            //    if(num % 2 == 0)
+            //    {
+            //        numerosPares.Add(num);
+            //    }
+            //}
+            //IEnumerable<int> numerosPares = from numero in numeros where numero % 2 == 0 select numero;
+            //foreach (int num in numerosPares)
+            //{
+            //    Console.WriteLine(num);
+            //}
+
+            ControlEmpresaEmpleado cee = new ControlEmpresaEmpleado();
+            //cee.MostrarCEOS();
+            //cee.MostrarEmpleadosOrdenados();
+            //cee.MostrarEmpleadosFacebook();
+
+            Console.Write("Ingresa el id de la empresa: ");
+            try
+            {
+                int idEmpresa = int.Parse(Console.ReadLine());
+                cee.MostrarEmpleadosPorIdEmpresa(idEmpresa);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+
+            Console.WriteLine();
+            Console.WriteLine("FIN DEL PROGRAMA");
+            Console.ReadKey();
+        }
+    }
+
+    class ControlEmpresaEmpleado
+    {
+        public List<Empresa> listaEmpresas;
+        public List<Empleado> listaEmpleados;
+
+        public ControlEmpresaEmpleado()
+        {
+            this.listaEmpresas = new List<Empresa>();
+            this.listaEmpleados = new List<Empleado>();
+
+            this.listaEmpresas.Add(new Empresa { Id = 1, Nombre = "Facebook" });
+            this.listaEmpresas.Add(new Empresa { Id = 2, Nombre = "Twitter" });
+            this.listaEmpleados.Add(new Empleado { Id = 1, Nombre = "Emma Barrios", Cargo = "CEO", IdEmpresa = 1, Salario = 200000 });
+            this.listaEmpleados.Add(new Empleado { Id = 2, Nombre = "Luna Perez", Cargo = "CEO", IdEmpresa = 2, Salario = 200000 });
+            this.listaEmpleados.Add(new Empleado { Id = 3, Nombre = "Enzo Fernandez", Cargo = "Co-CEO", IdEmpresa = 1, Salario = 200000 });
+            this.listaEmpleados.Add(new Empleado { Id = 4, Nombre = "Julian Alvarez", Cargo = "Co-CEO", IdEmpresa = 2, Salario = 200000 });
+        }
+
+        public void MostrarCEOS()
+        {
+            IEnumerable<Empleado> ceos = from empleado in this.listaEmpleados where empleado.Cargo == "CEO" select empleado;
+            foreach (Empleado emp in ceos)
+            {
+                emp.MostrarDatosEmpleado();
+            }
+        }
+
+        public void MostrarEmpleadosOrdenados()
+        {
+            IEnumerable<Empleado> empleados = from empleado in this.listaEmpleados orderby empleado.Nombre select empleado;
+            foreach (Empleado emp in empleados)
+            {
+                emp.MostrarDatosEmpleado();
+            }
+        }
+
+        public void MostrarEmpleadosFacebook()
+        {
+            IEnumerable<Empleado> empleados = from empleado in this.listaEmpleados
+                                              join empresa in this.listaEmpresas
+                                              on empleado.IdEmpresa equals empresa.Id
+                                              where empresa.Nombre == "Facebook"
+                                              select empleado;
+            foreach (Empleado emp in empleados)
+            {
+                emp.MostrarDatosEmpleado();
+            }
+        }
+
+        public void MostrarEmpleadosPorIdEmpresa(int id)
+        {
+            IEnumerable<Empleado> empleados = from empleado in this.listaEmpleados
+                                              join empresa in this.listaEmpresas
+                                              on empleado.IdEmpresa equals empresa.Id
+                                              where empresa.Id == id
+                                              select empleado;
+            foreach (Empleado emp in empleados)
+            {
+                emp.MostrarDatosEmpleado();
+            }
+        }
+    }
+
+    class Empresa
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+
+        public void MostrarDatosEmpresa()
+        {
+            Console.WriteLine($"Empresa {this.Nombre} con Id {this.Id}.");
+        }
+    }
+
+    class Empleado
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Cargo { get; set; }
+        public double Salario { get; set; }
+        public int IdEmpresa { get; set; }
+
+        public void MostrarDatosEmpleado()
+        {
+            Console.WriteLine($"Empleado {this.Nombre} con Id {this.Id}, cargo {this.Cargo}, salario {this.Salario} y pertenece a la empresa {this.IdEmpresa}");
+        }
+    }
+}
